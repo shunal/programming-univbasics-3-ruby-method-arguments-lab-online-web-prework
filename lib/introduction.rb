c3af42c54dc78ec64055de83fb9e7c586b140853
Steven Hunal <stevehunal@gmail.com>
2019-06-27 19:53:34 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,5 +7,5 @@ def introduction_with_language (name, language)
 end
 
 def introduction_with_language_optional (name, language)
-  puts 
-end
+  puts "Hi, my name is #{name} and I am learning to program in Ruby.
+end 
